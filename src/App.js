b5a6d82a6b6aca9091e92a041a5adfd8db10294a
HyungMinKang <ramkid91@gmail.com>
2020-05-19 15:01:02 +0900

    finishing use api get data

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-// function component ----> class component
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  //   setState를 사용하는 이유- state에 변화를 줌(=new state 반환)과 동시에 render functiond이 호출될 수 있도록 하기 위해
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
+
+  //   비동기화 async - await - axios 로딩될 때까지 기다려라
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
-    console.log("component rendered");
+    this.getMovies();
   }
-  componentDidUpdate() {
-    console.log("I'm updated");
-  }
-
   render() {
-    console.log("I am rendering");
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <section class="container">
+        {/* state를 통해 로딩/ 준비완료 표시 */}
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader_text">Loading ...</span>
+          </div>
+        ) : (
+          // map을 통해 api에서 받은 데이터를 내가 쓰고자하는 형식에 맞게 가져온후 새로운 Movie로 반환
+          movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          ))
+        )}
+      </section>
     );
   }
 }
